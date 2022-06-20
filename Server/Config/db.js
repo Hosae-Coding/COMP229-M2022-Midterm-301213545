f60a00b71509c2d9d50ade65680e8f5d7f2e4b89
LAPTOP-T21LK98P\KHS <https://github.com/Hosae-Coding>
2022-06-20 18:57:18 -0400

    add

---
+++ Server/Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/books229";
+exports.RemoteURI = "mongodb+srv://hongseok:f6T3J9cbbqR36H5V@cluster0.yfbvu.mongodb.net/book?retryWrites=true&w=majority";
+exports.HostName = "LocalHost";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/books229";
+export const RemoteURI = "mongodb+srv://hongseok:f6T3J9cbbqR36H5V@cluster0.yfbvu.mongodb.net/book?retryWrites=true&w=majority"
+export const HostName = "LocalHost";
+export const Secret = "someSecret";
